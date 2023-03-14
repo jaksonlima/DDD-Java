dd3bef0f0ab2d5a3c04e32ade7d44c0407cc3098
jaksonlima <jaksonsneider@gmail.com>
2023-03-14 00:23:43 -0300

    JPA

--- infrastructure/src/main/java/com/domain/driver/designer/infrastructure/category/CategoryConfigurationBean.java
+++ infrastructure/src/main/java/com/domain/driver/designer/infrastructure/category/CategoryConfigurationBean.java
@@ -1,2 +1,20 @@
-package com.domain.driver.designer.infrastructure.category;public class CategoryConfigurationBean {
+package com.domain.driver.designer.infrastructure.category;
+
+import com.domain.driver.designer.application.category.create.CreateCategoryUseCase;
+import com.domain.driver.designer.application.category.create.DefaultCreateCategoryUseCase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Service
+public class CategoryConfigurationBean {
+
+    @Bean
+    public CreateCategoryUseCase createCategoryUseCase(final CategoryMySQLGateway categoryMySQLGateway) {
+        Objects.requireNonNull(categoryMySQLGateway);
+
+        return new DefaultCreateCategoryUseCase(categoryMySQLGateway);
+    }
+
 }
