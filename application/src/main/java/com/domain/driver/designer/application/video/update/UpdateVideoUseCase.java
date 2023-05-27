30c7ca376749c1740eec2051c2f15c51f0b98c51
jaksonlima <jaksonsneider@gmail.com>
2023-05-27 15:16:41 -0300

    Video Test #4

---
+++ application/src/main/java/com/domain/driver/designer/application/video/update/UpdateVideoUseCase.java
@@ -0,0 +1,7 @@
+package com.domain.driver.designer.application.video.update;
+
+import com.domain.driver.designer.application.UseCase;
+
+public abstract class UpdateVideoUseCase
+        extends UseCase<UpdateVideoCommand, UpdateVideoOutput> {
+}
