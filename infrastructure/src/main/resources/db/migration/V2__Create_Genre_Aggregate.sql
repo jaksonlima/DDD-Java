c1dfe5554b5cc262d4572ead935334f091d34042
jaksonlima <jaksonsneider@gmail.com>
2023-06-01 23:57:14 -0300

    Migrate Refactory Category

--- infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
+++ infrastructure/src/main/resources/db/migration/V2__Create_Genre_Aggregate.sql
@@ -13,5 +13,5 @@ CREATE TABLE genres_categories (
     category_id CHAR(32) NOT NULL,
     CONSTRAINT idx_genres_categories UNIQUE (genre_id, category_id),
     CONSTRAINT idx_genre_id FOREIGN KEY (genre_id) REFERENCES genres (id) ON DELETE CASCADE,
-    CONSTRAINT idx_category_id FOREIGN KEY (category_id) REFERENCES category (id) ON DELETE CASCADE
+    CONSTRAINT idx_category_id FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE CASCADE
 );
